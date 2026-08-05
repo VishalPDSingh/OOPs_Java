84f16fa62eb0820631ddd1b24a321ede6ea9e368
VishalPDSingh <vishalprasadsingh4@gmail.com>
2026-08-05 17:54:07 +0530

    first commit

--- ch_CollectionFrameWork/iterable_Interface/Demo1.java
+++ ch_CollectionFrameWork/iterable_Interface/Demo1.java
@@ -1,7 +1,24 @@
 package ch_CollectionFrameWork.iterable_Interface;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class Demo1 {
     public static void main(String[] args) {
         
+        List<Integer> arr = new ArrayList<>();
+        arr.add(10);
+        arr.add(20);
+        arr.add(30);
+        arr.add(40);
+        arr.add(50);
+        arr.add(60);
+
+        Iterator<Integer> it = arr.iterator();
+        while(it.hasNext())
+        {
+            System.out.println(it.next());
+        }
     }
 }
