5918eafd97ee08fd571f2bbd6ff8a8a24a71be33
VishalPDSingh <vishalprasadsingh4@gmail.com>
2026-08-05 18:00:26 +0530

    first commit

---
+++ ch1_ClassAndObject/Demo1.java
@@ -0,0 +1,25 @@
+package ch1_ClassAndObject;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        
+        
+    }
+}
+
+
+
+class Car
+{
+    String name;
+    String colur;
+    double price;
+    void start()
+    {
+        System.out.println("Car is started");
+    }
+    void stop()
+    {
+        System.out.println("Car is stop");
+    }
+}
